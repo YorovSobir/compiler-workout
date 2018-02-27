43fac10652677413f6854e19cc2b39a158322679
YorovSobir <sobir.yorov94@gmail.com>
2018-02-27 22:30:58 +0300

    sm eval impl

--- src/SM.ml
+++ src/SM.ml
@@ -1,15 +1,14 @@
-open GT       
-       
+open GT
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
-(* put a constant on the stack     *) | CONST of int                 
+(* put a constant on the stack     *) | CONST of int
 (* read to stack                   *) | READ
 (* write from stack                *) | WRITE
 (* load a variable to the stack    *) | LD    of string
 (* store a variable from the stack *) | ST    of string with show
 
-(* The type for the stack machine program *)                                                               
+(* The type for the stack machine program *)
 type prg = insn list
 
 (* The type for the stack machine configuration: a stack and a configuration from statement
@@ -22,8 +21,47 @@ type config = int list * Syntax.Stmt.config
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+ *)
+
+let evalHelper conf prog confRes =
+    match conf with
+        (stack, stmtConf) -> (
+            match prog with
+                | [] -> confRes
+                | ins :: insxs -> (
+                    match ins with
+                        | BINOP op -> (
+                            match stack, stmtConf with
+                                | y :: x :: st, (exprSt, ixs, oxs) ->
+                                    ((Syntax.Expr.eval exprSt (Syntax.Expr.Binop (op, Syntax.Expr.Const x, Syntax.Expr.Const y))) :: st, stmtConf)
+                                | _ -> failwith @@ "not enough args in stack"
+                        )
+                        | CONST num -> (num :: stack, stmtConf)
+                        | READ -> (
+                            match stmtConf with
+                                | (exprSt, z :: ixs, oxs) -> (z :: stack, (exprSt, ixs, oxs))
+                                | _ -> failwith @@ "input int list are empty"
+                        )
+                        | WRITE -> (
+                            match stack, stmtConf with
+                                | z :: st, (exprSt, ixs, oxs) -> (st, (exprSt, ixs, oxs@[z]))
+                                | _ -> failwith @@ "stack is empty"
+                        )
+                        | LD x -> (
+                            match stmtConf with
+                                | (exprSt, ixs, oxs) -> ((exprSt x) :: stack, stmtConf)
+                        )
+                        | ST x -> (
+                            match stack, stmtConf with
+                                | z :: st, (exprSt, ixs, oxs) -> (st, (Syntax.Expr.update x z exprSt, ixs, oxs))
+                                | _ -> failwith @@ "stack is empty"
+                        )
+                        | _ -> failwith @@ "temp"
+                )
+
+        )
+
+let eval conf prog = evalHelper conf prog ([], conf)
 
 (* Stack machine compiler
 
--- src/Syntax.ml
+++ src/Syntax.ml
@@ -1,14 +1,14 @@
 (* Opening a library for generic programming (https://github.com/dboulytchev/GT).
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
-open GT 
-    
+open GT
+
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
-    
-    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
-       notation, it came from GT. 
+
+    (* The type for expressions. Note, in regular OCaml there is no "@type..."
+       notation, it came from GT.
     *)
     @type t =
     (* integer constant *) | Const of int
@@ -22,14 +22,14 @@ module Expr =
         +, -                 --- addition, subtraction
         *, /, %              --- multiplication, division, reminder
     *)
-                                                            
+
     (* State: a partial map from variables to integer values. *)
-    type state = string -> int 
+    type state = string -> int
 
     (* Empty state: maps every variable into nothing. *)
     let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-    (* Update: non-destructively "modifies" the state s by binding the variable x 
+    (* Update: non-destructively "modifies" the state s by binding the variable x
       to value v and returns the new state.
     *)
     let update x v s = fun y -> if x = y then v else s y
@@ -37,8 +37,8 @@ module Expr =
     (* Expression evaluator
 
           val eval : state -> t -> int
- 
-       Takes a state and an expression, and returns the value of the expression in 
+
+       Takes a state and an expression, and returns the value of the expression in
        the given state.
     *)
     let int_of_bool b = if b then 1 else 0
@@ -68,7 +68,7 @@ module Expr =
 	| _ -> failwith "unsupported expression"
 
   end
-                    
+
 (* Simple statements: syntax and sematics *)
 module Stmt =
   struct
@@ -81,7 +81,7 @@ module Stmt =
     (* composition                      *) | Seq    of t * t with show
 
     (* The type of configuration: a state, an input stream, an output stream *)
-    type config = Expr.state * int list * int list 
+    type config = Expr.state * int list * int list
 
     (* Statement evaluator
 
@@ -89,6 +89,20 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
-                                                         
+    let rec eval conf stmt =
+        match stmt with
+            | Read x -> (
+                match conf with
+                    | (_, [], _) -> failwith @@ "empty int list"
+                    | (exprSt, z :: ixs, oxs) -> (Expr.update x z exprSt, ixs, oxs)
+            )
+            | Write exprT -> (
+                match conf with
+                    | (exprSt, ixs, oxs) -> (exprSt, ixs, oxs@[(Expr.eval exprSt exprT)])
+            )
+            | Assign (x, exprT) -> (
+                match conf with
+                    | (exprSt, ixs, oxs) -> (Expr.update x (Expr.eval exprSt exprT) exprSt, ixs, oxs)
+            )
+            | Seq (stmt1, stmt2) -> eval (eval conf stmt1) stmt2
   end
